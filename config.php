ef7609c91ddcf8e939a17668dba470625439ed72
shahmon <shah@waybeo.com>
2013-09-08 19:29:17 +0200

    sitefiles

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://aloo.malapadakkam.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://aloo.malapadakkam.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/aloo.malapadakkam.com/catalog/');
+define('DIR_SYSTEM', '/var/www/aloo.malapadakkam.com/system/');
+define('DIR_DATABASE', '/var/www/aloo.malapadakkam.com/system/database/');
+define('DIR_LANGUAGE', '/var/www/aloo.malapadakkam.com/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/aloo.malapadakkam.com/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/aloo.malapadakkam.com/system/config/');
+define('DIR_IMAGE', '/var/www/aloo.malapadakkam.com/image/');
+define('DIR_CACHE', '/var/www/aloo.malapadakkam.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/aloo.malapadakkam.com/download/');
+define('DIR_LOGS', '/var/www/aloo.malapadakkam.com/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'aloobombuser');
+define('DB_PASSWORD', 'AlooBomb!042');
+define('DB_DATABASE', 'aloobomb');
+define('DB_PREFIX', 'ac_');
+?>
